4d572bc22c458f3a5f6eff51c8d1e10c93d5ff2e
Computer <home@Computers-MacBook-Pro.local>
2019-11-16 11:13:57 -0600

    login page clean up

--- js/authenticate.js
+++
@@ -1,6 +0,0 @@
-function parseJson(){  
- $.getJSON("http://localhost:8080/web%20development//data/demographic_info.json",function(json) {
-        var address = json.members[0].firstName;
-        alert(address);
-        });
-}   
